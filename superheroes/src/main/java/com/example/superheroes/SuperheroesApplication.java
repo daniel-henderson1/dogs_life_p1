eb50685948c6a19bc99fbfe97dab0dd98063f1ce
Mark Muthalaly <markgmuthalaly@gmail.com>
2023-07-28 20:25:38 +0100

    QL3 and QL4 completed

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,22 +1,29 @@
 package com.example.superheroes;
 
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.repository.HeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.superheroes.practice.Calculator;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
 	@Autowired
-	Calculator calc;
+	HeroRepository heroRepository;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 	@Override
 	public void run(String... args) throws Exception {
-      System.out.println("Hello world!");
-	  int result = calc.addition(5,6);
-		System.out.println(result);
+		List<Hero> heroes = heroRepository.findAll();
+		for (Hero hero: heroes){
+			System.out.println(hero);
+		}
 	}
 }
