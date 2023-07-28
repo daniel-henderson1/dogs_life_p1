b73abf222866544843c843bab27c2fc0fbd0b85d
Saizana Asmelash <mail@db.com>
2023-07-28 18:53:22 +0100

    Springboot 1

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,19 +1,22 @@
 package com.example.superheroes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.superheroes.practice.Calculator;
 
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
-
+	@Autowired
+	Calculator calc;
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 	@Override
 	public void run(String... args) throws Exception {
       System.out.println("Hello world!");
+	  int result = calc.addition(5,6);
+		System.out.println(result);
 	}
-
-
 }
